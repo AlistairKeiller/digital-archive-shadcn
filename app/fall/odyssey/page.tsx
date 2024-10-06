7b390fb109e0074348f0d23a4130262369546f79
Alistair Keiller <alistair@keiller.net>
2024-10-05 23:25:07 -0700

    test

--- app/fall/odyssey/page.tsx
+++ app/fall/odyssey/page.tsx
@@ -2,7 +2,10 @@ import {ProfileCard} from "@/components/profilecard";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-col">
+        testing
+      </div>
     </div>
   );
 }
