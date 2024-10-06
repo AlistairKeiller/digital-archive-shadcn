259d26fc97a37a8590bca6627f77e5d776b2819a
Alistair Keiller <alistair@keiller.net>
2024-10-06 15:56:50 -0700

    test

--- components/profilecard.tsx
+++ components/profilecard.tsx
@@ -6,7 +6,7 @@ import zipperMouth from '@/components/zippermouth.svg';
 export function ProfileCard() {
     return (
         <div className="flex justify-center items-center min-h-[calc(100vh-69px)] bg-gray-100">
-            <Card className="w-full bg-white shadow-lg rounded-lg">
+            <Card className="max-w-4xl w-full bg-white shadow-lg rounded-lg">
                 {/* Profile Header */}
                 <CardHeader className="flex flex-col items-center p-6 border-b">
                     <Image
@@ -27,15 +27,16 @@ export function ProfileCard() {
                         <div className="w-full md:w-1/2">
                             <h3 className="text-lg font-semibold mb-2">Work</h3>
                             <p className="text-gray-700">
-                                I'm kinda obsessed with making things, which has lead me down countless rabbit holes in both academic and industry. I have a contrail of 73 (and counting) programming projects on github, I've taken a decent number of math, programming, and philosophy classes for fun (through duel enrollment programs), and I'm a part of a startup working on a device that helps with patient recovery (I wish I could say more, but NDAs and such are all <Image src={zipperMouth} alt="" height={25} width={25} className="inline-block" /> ).
+                                I'm kinda obsessed with making things, which has lead me down countless rabbit holes in both academic and industry. I have a contrail of 73 (and counting) programming projects on github, I've taken a decent number of math, programming, and philosophy classes for fun (through duel enrollment programs), and I'm a part of a startup working on a device that helps with patient recovery (I wish I could say more, but NDAs and such are all <Image src={zipperMouth} alt="" height={25} width={25} className="inline-block"></Image> ).
                             </p>
                         </div>
+
 
                         {/* Personal Section */}
                         <div className="w-full md:w-1/2">
                             <h3 className="text-lg font-semibold mb-2">Life</h3>
                             <p className="text-gray-700">
-                                I spend a lot of my free time on semi-academic stuff (I have a bit of a maker's spirit, so robotics and programming are very addictive), because I find them really fun. But when I do need a break, I love going hiking (I'm in UCI hiking club, which is an absolute blast, and I hiked a sizable chunk of the A.T.) and sailing hobie cats around bays (Mission Bay in San Diego, and also the surrounding sea).
+                                I spend a lot of my free time on semi-academic stuff (I have a bit of a maker's spirit, so robotics and programming and very addictive), because I find them really fun. But when I do need a break, I love going hiking (I'm in UCI hiking club, which is an absolute blast, and I hiked a sizable chunk of the A.T.) and sailing hobie cats around bays (Mission Bay in san diego, and also the surrounding sea).
                             </p>
                         </div>
                     </div>
