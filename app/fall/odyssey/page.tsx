be2cf2a92921795f7ff54c0d2b10e01fffaeb825
Alistair Keiller <alistair@keiller.net>
2024-10-06 00:05:28 -0700

    add

--- app/fall/odyssey/page.tsx
+++ app/fall/odyssey/page.tsx
@@ -1,10 +1,13 @@
-import {ProfileCard} from "@/components/profilecard"; 
+import { ProfileCard } from "@/components/profilecard";
 
 export default function Home() {
   return (
     <div className="flex justify-center min-h-screen bg-gray-100">
-      <div className="flex flex-col">
-        testing
+      <div className="flex flex-col text-center space-y-4 mt-4">
+        <h1 className="text-4xl font-bold text-gray-900">The Duality of Odyssey: <br/> Technology & Wit</h1>
+        <p className="text-xl text-gray-600 max-w-2xl">
+        Let's look at the Odyssey, while looking at both the perspective of Odysseus' rhetoric, and that rhetorics technological implications.
+        </p>
       </div>
     </div>
   );
