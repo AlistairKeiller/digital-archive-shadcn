9759860c9e59940c55e0422ca8e2e36f4ffd902a
Alistair Keiller <alistair@keiller.net>
2024-10-06 00:30:06 -0700

    update

--- app/fall/odyssey/page.tsx
+++ app/fall/odyssey/page.tsx
@@ -2,7 +2,7 @@ import { FaCog, FaLightbulb } from "react-icons/fa"; // Import icons from react-
 
 export default function Home() {
   return (
-    <div className="flex justify-center min-h-screen bg-gray-100">
+    <div className="flex justify-center min-h-[calc(100vh-69px)] bg-gray-100">
       <div className="flex flex-col text-center space-y-4 mt-4">
         <h1 className="text-4xl font-bold text-gray-900">
           The Duality of Odyssey: <br />
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import {ProfileCard} from "@/components/profilecard";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <ProfileCard />
-    </div>
   );
 }
