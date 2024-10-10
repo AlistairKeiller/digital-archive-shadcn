49b09a3acfaf7755332a90d1ecf76dbd3333169b
Alistair Keiller <alistair@keiller.net>
2024-10-09 21:02:03 -0700

    update

--- app/fall/odyssey/page.tsx
+++ app/fall/odyssey/page.tsx
@@ -15,6 +15,7 @@ import Calypso from "@/images/Calypso.jpg";
 import Begging from "@/images/Begging.jpg";
 import Kleos from "@/images/Kleos.jpg";
 import Swan from "@/images/Swan.jpg";
+import Nostos from "@/images/Nostos.jpg";
 import Image from "next/image";
 
 export default function Home() {
@@ -187,48 +188,65 @@ export default function Home() {
               </PopoverContent>
             </Popover>" himself. Odysseus is a widely renowned hero, with no devine blood or specific devine honors to attribute his title to (Athena is his patreon, which helps his ends, but isn't a specific enough award to justify the heroic title by divine definition).
           </p>
-            <p className="leading-7 [&:not(:first-child)]:mt-6">
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
             Instead, I will turn to "The Ancient Greek Hero in 24 Hours" for a more complete picture: Gregory Nagy posits that a hero is defined by their "<Popover>
               <PopoverTrigger className="underline cursor-pointer">
-              Kleos
+                kleos
               </PopoverTrigger>
               <PopoverContent className="w-64 p-4">
-              <Image
-                src={Kleos}
-                alt="Kleos"
-                className="h-48 object-cover rounded-md mx-auto"
-              />
-              <p className="text-sm text-gray-600 text-center mt-2">
-                Kleos is a Greek word often translated to "glory" or "fame." It is the concept of what others hear about you, and how you are remembered after you die.
-              </p>
+                <Image
+                  src={Kleos}
+                  alt="kleos"
+                  className="h-48 object-cover rounded-md mx-auto"
+                />
+                <p className="text-sm text-gray-600 text-center mt-2">
+                  kleos is a Greek word often translated to "glory" or "fame." It is the concept of what others hear about you, and how you are remembered after you die.
+                </p>
               </PopoverContent>
-            </Popover>." According to Nagy, Greece was a song culture, with a emphasis on sorrow and death. So a hero's Kleos was proportional to the impact of the song representing their death, also known as their "<Popover>
+            </Popover>." According to Nagy, Greece was a song culture, with a emphasis on sorrow and death. So a hero's kleos was proportional to the impact of the song representing their death, also known as their "<Popover>
               <PopoverTrigger className="underline cursor-pointer">
-              swan song
+                swan song
               </PopoverTrigger>
               <PopoverContent className="w-64 p-4">
-              <Image
-                src={Swan}
-                alt="Swan"
-                className="h-48 object-cover rounded-md mx-auto"
-              />
-              <p className="text-sm text-gray-600 text-center mt-2">
-                In Greek mythology, swans were believed to sing a beautiful song just before they died, symbolizing a poignant and memorable end. The "swan song" of a person is considered their "The Living Word," or the last words that a person perpetuates.
-              </p>
+                <Image
+                  src={Swan}
+                  alt="Swan"
+                  className="h-48 object-cover rounded-md mx-auto"
+                />
+                <p className="text-sm text-gray-600 text-center mt-2">
+                  In Greek mythology, swans were believed to sing a beautiful song just before they died, symbolizing a poignant and memorable end. The "swan song" of a person is considered their "The Living Word," or the last words that a person perpetuates.
+                </p>
               </PopoverContent>
             </Popover>."
             In fact, according to Nagy, "the precise moment when everything comes together for the hero is the moment of death."
             So there was a massive amount of pressure to "<Popover>
               <PopoverTrigger className="underline cursor-pointer">
-              Script
+                Script
               </PopoverTrigger>
               <PopoverContent className="w-64 p-4">
-              <p className="text-sm text-gray-600">
-                A hero is considered <span className="italic">unseasonal</span> for their entire lifetime, so "scripting" their death is all about nailing the timing to have a <span className="italic">seasonal</span>/heroic death. The underlying etymology of <span className="italic">hērōs</span> that justify this fact is unbelievably insightful, but also beyond the scope of this post, but I deeply encurage you to read Hero in 24 Hours!!
-              </p>
+                <p className="text-sm text-gray-600">
+                  A hero is considered <span className="italic">unseasonal</span> for their entire lifetime, so "scripting" their death is all about nailing the timing to have a <span className="italic">seasonal</span>/heroic death. The underlying etymology of <span className="italic">hērōs</span> that justify this fact is unbelievably insightful, but also beyond the scope of this post, but I deeply encurage you to read Hero in 24 Hours!!
+                </p>
               </PopoverContent>
             </Popover>" one's dying words to create a lasting legacy, and immortalize themselves as a hero.
-            </p>
+          </p>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            Although Greek kleos is often derived from their swan song, Odysseus' is remembered by his "<Popover>
+              <PopoverTrigger className="underline cursor-pointer">
+                nostos
+              </PopoverTrigger>
+              <PopoverContent className="w-64 p-4">
+                <Image
+                  src={Nostos}
+                  alt="nostos"
+                  className="h-48 object-cover rounded-md mx-auto"
+                />
+                <p className="text-sm text-center text-gray-600">
+                  Nostos is a Greek word meaning "homecoming" or "return." It refers to the hero's journey back home after a long adventure, often filled with trials and tribulations.
+                </p>
+              </PopoverContent>
+            </Popover>."
+          </p>
         </div>
       </div>
     </div>
