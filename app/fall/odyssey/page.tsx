503a42bd3c846a7ffa9d66eb0124142025b9156b
Alistair Keiller <alistair@keiller.net>
2024-10-09 21:27:21 -0700

    update

--- app/fall/odyssey/page.tsx
+++ app/fall/odyssey/page.tsx
@@ -252,6 +252,9 @@ export default function Home() {
             <iframe width="560" height="315" src="https://www.youtube.com/embed/xGZ4vLNmGAU?si=1ejzKUP5DcVGal6I" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
           </div>
         </div>
+        <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-01">
+          The Contradiction
+        </h2>
       </div>
     </div>
   );
