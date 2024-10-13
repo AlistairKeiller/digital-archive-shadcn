e97df585c580e19c4c21c6df810fe21d35149426
Alistair Keiller <alistair@keiller.net>
2024-10-12 21:23:27 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import {ProfileCard} from "@/components/profilecard"; 
-import {Atla} from "@/components/atla";
-
 
 export default function Home() {
 
   return (
       <div className="flex flex-col">
         <ProfileCard />
-        <Atla />
       </div>
   );
 }
--- components/map.tsx
+++ components/map.tsx
@@ -1,6 +1,12 @@
 "use client";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import dynamic from 'next/dynamic';
 import 'leaflet/dist/leaflet.css';
+
+// Dynamically import the Map component with SSR disabled
+const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
+const Marker = dynamic(() => import('react-leaflet').then(mod => mod.Marker), { ssr: false });
+const Popup = dynamic(() => import('react-leaflet').then(mod => mod.Popup), { ssr: false });
 
 export default function Map() {
     return (
