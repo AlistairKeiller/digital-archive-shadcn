d423c258cafeb2b79dfce6d81bd4d72a3c5e8a4b
Alistair Keiller <alistair@keiller.net>
2024-11-19 12:51:13 -0800

    update

--- app/fall/decameron/page.tsx
+++ app/fall/decameron/page.tsx
@@ -7,22 +7,14 @@ export default function Decameron() {
     <div className="flex justify-center min-h-[calc(100vh-69px)]">
       <div className="mt-6 mx-4 max-w-3xl">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl flex justify-center">
-          <div className="flex">
-            Nesting
-            <div className="ml-6 flex justify-center items-center relative h-10 overflow-hidden leading-10 w-72">
-              <div className="absolute top-0 w-full text-center">
-                <span className="absolute animate-roll font-bold flex flex-col items-center justify-center">
-                  Worlds
-                  <br />
-                  Worlds
-                  <br />
-                  Worlds
-                  <br />
-                  Worlds
-                </span>
+            <div className="mr-72">
+              Nesting{` `}
+              <div className="text-box inline-block relative">
+                <div className="inline-block absolute rotate-[-90] opacity-0">Worlds</div>
+                <div className="inline-block absolute rotate-[-90] opacity-0">Worlds</div>
+                <div className="inline-block absolute rotate-[-90] opacity-0">Worlds</div>
               </div>
             </div>
-          </div>
         </h1>
         <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
           The Decameron's Worldbuilding
@@ -35,6 +27,89 @@ export default function Decameron() {
           connected worlds.
         </p>
       </div>
+      <style jsx>{`
+        .rotate-\[-90\] {
+          transform: rotateX(-90deg);
+        }
+
+        .text-box div:nth-child(1){
+          animation: rollDown 10s forwards infinite;
+        }
+
+        .text-box div:nth-child(2){
+          animation: rollDown2 10s forwards infinite;
+        }
+
+        .text-box div:nth-child(3){
+          animation: rollDown3 10s forwards infinite;
+        }
+
+        @keyframes rollDown {
+          0%{
+            top: -200px;
+            transform: rotateX(-90deg);
+          }
+          11%{
+            top: -39px;
+            transform: rotateX(0deg);
+            opacity: 1;
+          }
+          22%{
+            top: -39px;
+            transform: rotateX(0deg);
+            opacity: 1;
+          }
+          33%{
+            top: 0px;
+            transform: rotateX(30deg);
+            opacity: 0;
+          }
+        }
+
+        @keyframes rollDown2 {
+          33%{
+            top: -200px;
+            transform: rotateX(-90deg);
+          }
+          44%{
+            top: -39px;
+            transform: rotateX(0deg);
+            opacity: 1;
+          }
+          55%{
+            top: -39px;
+            transform: rotateX(0deg);
+            opacity: 1;
+          }
+          66%{
+            top: 0px;
+            transform: rotateX(30deg);
+            opacity: 0;
+          }
+        }
+
+        @keyframes rollDown3 {
+          66%{
+            top: -200px;
+            transform: rotateX(-90deg);
+          }
+          77%{
+            top: -39px;
+            transform: rotateX(0deg);
+            opacity: 1;
+          }
+          88%{
+            top: -39px;
+            transform: rotateX(0deg);
+            opacity: 1;
+          }
+          99%{
+            top: 0px;
+            transform: rotateX(30deg);
+            opacity: 0;
+          }
+        }
+      `}</style>
     </div>
   );
 }
