5c38782ca43dd4b52bbd9f5fb10a1f054618abf8
Alistair Keiller <alistair@keiller.net>
2024-11-19 01:08:00 -0800

    update

--- app/fall/decameron/page.tsx
+++ app/fall/decameron/page.tsx
@@ -1,26 +1,14 @@
 "use client";
-import React, { useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { Text } from "@react-three/drei";
-import { useSpring, a } from "@react-spring/three";
 
 export default function Decameron() {
   return (
     <div className="flex justify-center min-h-[calc(100vh-69px)]">
       <div className="mt-6 mx-4 max-w-3xl">
-        <Canvas style={{ height: "60px" }}>
-          <Text
-            position={[0, 0, 0]}
-            fontSize={6}
-            color="black"
-            anchorX="center"
-            anchorY="middle"
-          >
-            Where is Worldbuilding Possible?
-          </Text>
-        </Canvas>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+          Nested Worlds
+        </h1>
         <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-          The Decameron's worldbuilding
+          The Decameron's Worldbuilding
         </h2>
         <p className="leading-7 [&:not(:first-child)]:mt-6">
           The Decameron builds its world in between the stories, using their
