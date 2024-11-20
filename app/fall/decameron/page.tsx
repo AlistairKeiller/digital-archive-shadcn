8abb819c3d49ba19d7da0691c46b0c909fe3985e
Alistair Keiller <alistair@keiller.net>
2024-11-19 16:13:12 -0800

    fix

--- app/fall/decameron/page.tsx
+++ app/fall/decameron/page.tsx
@@ -17,7 +17,7 @@ export default function Decameron() {
             </div>
         </h1>
         <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-          The Decameron's Worldbuilding
+          The Decameron&apos;s Worldbuilding
         </h2>
         <p className="leading-7 [&:not(:first-child)]:mt-6">
           The Decameron builds its world in between the stories, using their
